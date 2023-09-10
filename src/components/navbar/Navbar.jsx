a17a56b8f091b313c18fe75a1ef6d7df69265c3c
tpauldike <topman4loveworld@gmail.com>
2023-09-10 22:28:18 +0000

    implemented navbar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
-// import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -18,6 +19,21 @@ const Navbar = () => {
           <p><a href="#features">Case Study</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">\
+          <div className="gpt3__navbar-menu_container-links"></div>
+          </div>
+          )}
       </div>
     </div>
   );
