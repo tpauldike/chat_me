7eca2c25478440cadec970ce0e6122b3f5f3aa22
tpauldike <topman4loveworld@gmail.com>
2023-09-16 01:12:27 +0000

    Completed the Header component and made it responsive

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -25,12 +25,12 @@ const Header = () => {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-
-        <div className="gpt3__header-image">
-            <img src={ai} alt="AI" />
-        </div>
-
       </div>
+
+      <div className="gpt3__header-image">
+        <img src={ai} alt="AI" />
+      </div>
+      
     </div>
   );
 };
